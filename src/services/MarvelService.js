b297aa6f830f345d1fbc710c4cd5020883d7c320
andreykas <andreykaas29@gmail.com>
2024-03-25 17:57:30 +0200

    loading, marverService

---
+++ src/services/MarvelService.js
@@ -0,0 +1,36 @@
+
+class MarverService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=a2cbeb32ee66a6e200e8c20c247dde4f';
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.url,
+            wiki: char.urls[1].url
+        }
+    } 
+}
+
+export default MarverService;
